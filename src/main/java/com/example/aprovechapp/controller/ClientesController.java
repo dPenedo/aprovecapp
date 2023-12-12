d9d35ed91035628840ea9b7464db7caa855181eb
dPenedo <dpenedo@tutanota.com>
2023-12-11 23:03:32 -0300

    implementando thymeleaf pero no funciona bootstrap y en hava 21 no corre bien

--- src/main/java/com/example/aprovechapp/controller/ClientesController.java
+++ src/main/java/com/example/aprovechapp/controller/ClientesController.java
@@ -5,9 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class ClientesController {
 //    @Autowired
